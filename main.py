9aba8c193dd934d4c4a7bce361c1a119a3a3b694
FedOS <76245715+legendaOS@users.noreply.github.com>
2023-08-08 19:22:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+
 from listToJSON import listToJSON
 from User import User
 from Game import Game
@@ -14,7 +16,7 @@ def main():
     return 'server works!'
 
 
-players = [ User('Вася', 'Пупкин', 1), User('Петя', 'Тряпкин', 2), User('Даша', 'Корйека', 3) ]
+players = [ User('Вася', 'Пупкин', 0), User('Петя', 'Тряпкин', 1), User('Даша', 'Корйека', 2) ]
 
 games = [
     Game(1, 'haha'),
@@ -42,6 +44,47 @@ def games_lsdlfkjsldkfsldkfj():
         'games': buffer,
         'count': len(buffer)
     }
+
+cats_names = ['Вася', 'Мурзик', 'Жирунчик', 'Вапек']
+cats_info = {
+    'Вася': 'дворняга', 
+    'Мурзик': 'домашний', 
+    'Жирунчик': 'сфинкс', 
+    'Вапек': 'британский'
+}
+
+@app.route('/cats')
+def cats():
+    return cats_names
+
+@app.route('/cat/<number>')
+def cat(number):
+    number = int(number)
+    if number < 0 or number > len(cats_names):
+        return 'неверный индекс!'
+    else:
+        return cats_names[number]
+
+@app.route('/cat_info/<name>')
+def cat_info(name):
+    if name in cats_info.keys():
+        return cats_info[name]
+    else:
+        return 'ошибка ключа!'
+    
+
+@app.route('/create_user')
+def create_user():
+    buffer_name = request.args.get('name')
+    buffer_sname = request.args.get('sname')
+
+    new_user = User(buffer_name, buffer_sname, len(players))
+
+    players.append(new_user)
+    return new_user.toJSON()
+
+
+
 
 
 if __name__ == '__main__':
