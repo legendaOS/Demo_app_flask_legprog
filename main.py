45fcc04d9d48687e0ab483f0b6ca1feb8e3a44d8
FedOS <76245715+legendaOS@users.noreply.github.com>
2023-08-27 19:39:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,76 +3,60 @@ from flask import request
 
 from listToJSON import listToJSON
 from User import User
-from Game import Game
 
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-    return 'server works!'
-
-
 players = [ User('Вася', 'Пупкин', 0), User('Петя', 'Тряпкин', 1), User('Даша', 'Корйека', 2) ]
 
-games = [
-    Game(1, 'haha'),
-    Game(2, 'hehe')
-]
-
-games[0].connect(players[0])
-games[0].connect(players[1])
-
-games[1].connect(players[2])
-games[1].connect(players[0])
-
-
-
-cats_names = ['Вася', 'Мурзик', 'Жирунчик', 'Вапек']
-
-
-@app.route('/cats', methods = ['GET'])
-def cats():
+@app.route('/users', methods = ['GET'])
+def users():
     return {
-        'count': len(cats_names),
-        'cats': cats_names
+        'count': len(players),
+        'users': listToJSON(players)
     }
 
-@app.route('/cat/<id>', methods = ['GET', 'PUT', 'DELETE'])
-def cat_id(id):
+@app.route('/user/<id>', methods = ['GET', 'PUT', 'DELETE'])
+def user_id(id):
     id = int(id)
 
     if request.method == "GET":
-        return cats_names[id]
+        return players[id].toJSON()
     
     elif request.method == "PUT":
         data = request.json
-        cats_names[id] = data['name']
+        players[id].name = data['name']
+        players[id].secname = data["secname"]
+        players[id].online = data["online"]
         return {
             'id': id,
-            'name': cats_names[id]
+            "user" : players[id].toJSON()
         }
     
     elif request.method == 'DELETE':
-        buffer_name = cats_names[id]
-        cats_names.pop(id)
+        buffer_name = players[id].name
+        players.pop(id)
         return {
             'id': id,
             'name': buffer_name
         }
-
-
-@app.route('/cat', methods = ['POST'])
-def cat():
+    
+@app.route('/user', methods = ['POST'])
+def user():
     if request.method == 'POST':
         data = request.json
-        cats_names.append(data['name'])
-        return {
-            'id': len(cats_names) - 1,
-            'name': data['name']
-        }
-
-
-
+        buf_User = User(data["name"], data["secname"], len(players))
+        players.append(buf_User)
+        return (
+                    {
+                        'id': len(players) - 1,
+                        'name': data['name']
+                    },
+                    256,
+                    {
+                    'maintainer': 'FixBananchik'
+                    }
+                )
+    
 if __name__ == '__main__':
     # app.run(port=3000, debug=True)
     app.run(port=3000)
